21d1afd9c52e77c34b4e06980799e44bd1298949
Koji <fursich0@gmail.com>
2017-01-27 15:59:38 +0000

    Twitterユーザープロフィール機能 （細かいリファクタリング､画面の調整､cssファイル分割など)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
     @q = Tweet.search(params[:q])
     @tweets = @q.result.where('user_id=?',params[:id]).page(params[:page]).per(10).order('created_at DESC')
   end
