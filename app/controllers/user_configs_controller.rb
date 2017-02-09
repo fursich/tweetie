b7df69192768c9697d89a7ae7549c19f6e455ef8
“Koji <fursich0@gmail.com>
2017-02-09 13:37:16 +0000

    herokuポスグレのデータがアップデートされていない問題対策(テスト用)2

--- app/controllers/user_configs_controller.rb
+++ app/controllers/user_configs_controller.rb
@@ -1,9 +1,10 @@
 class UserConfigsController < ApplicationController
+    skip_before_filter :verify_authenticity_token
 
     def create
         @user_conf = UserConfig.find_or_initialize_by(user_id: current_user.id)
-        @user_conf.show_replies = params[:show_replies]
-        @user_conf.show_followers_only = params[:show_followers_only]
+        @user_conf.attributes = user_conf_params
+        
         @user_conf.save!
         render nothing: true
     end
