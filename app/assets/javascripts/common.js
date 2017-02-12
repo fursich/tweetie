ab8f11a9ca11b68ed0f8d5853a2ea494bfed29cc
“Koji <fursich0@gmail.com>
2017-02-12 19:01:33 +0000

    ロゴ微修正

---
+++ app/assets/javascripts/common.js
@@ -0,0 +1,12 @@
+
+$(function(){
+
+    $('a.navbar-brand.header-logo-box').on('mouseenter', function(){
+        $(this).children('img.header-logo').attr('src',"./images/tweetie_logo_anime2.png");
+    });
+    $('a.navbar-brand.header-logo-box').on('mouseleave', function(){
+        $(this).children('img.header-logo').attr('src',"./images/tweetie_logo_final.png");
+    });
+
+})
+
