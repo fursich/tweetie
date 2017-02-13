f48713c4af579ffbde53e77fc2779a9bf1100972
“Koji <fursich0@gmail.com>
2017-02-13 03:31:31 +0000

    ロゴ画像パスを微修正

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -2,10 +2,10 @@
 $(function(){
 
     $('a.navbar-brand.header-logo-box').on('mouseenter', function(){
-        $(this).children('img.header-logo').attr('src',"./images/tweetie_logo_anime2.png");
+        $(this).children('img.header-logo').attr('src',"/images/tweetie_logo_anime2.png");
     });
     $('a.navbar-brand.header-logo-box').on('mouseleave', function(){
-        $(this).children('img.header-logo').attr('src',"./images/tweetie_logo_final.png");
+        $(this).children('img.header-logo').attr('src',"/images/tweetie_logo_final.png");
     });
 
 })
