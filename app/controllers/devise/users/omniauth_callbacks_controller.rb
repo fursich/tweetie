6d55b8d5cd2ae4e2af5149def6a65d251191461f
“Koji <fursich0@gmail.com>
2017-02-14 08:37:36 +0000

    test4-4

--- app/controllers/devise/users/omniauth_callbacks_controller.rb
+++ app/controllers/devise/users/omniauth_callbacks_controller.rb
@@ -22,7 +22,7 @@ class Devise::Users::OmniauthCallbacksController < Devise::OmniauthCallbacksCont
       set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
       sign_in_and_redirect @user, event: :authentication
     else
-      flash[:alert] = @user
+      flash[:alert] = @user.errors.full_messages
       session["devise.facebook_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
